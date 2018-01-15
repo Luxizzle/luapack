f129b58648cbe4b31b44955c6c38b71058d80e1e
Jelmer <luxizzle@gmail.com>
2018-01-15 21:05:58 +0100

    oof

--- test/packed.lua
+++ test/packed.lua
@@ -1,1 +1,39 @@
-local a={loaded={},packages={}}local b=require;function require(c)if a.loaded[c]then return a.loaded[c]elseif a.packages[c]then a.loaded[c]=a.packages[c]()return a.loaded[c]else return b(c)end end;a.packages['977c573b0324d59e85ec223a84801a3c']=function()print('Hello world!')require('9d731e9af32b646d81ee2c7d89663fbf')require('9d731e9af32b646d81ee2c7d89663fbf')require('5192f819e0978987d662edabaf3143e1')end;a.packages['9d731e9af32b646d81ee2c7d89663fbf']=function()print('hey there from module')return'hey there'end;a.packages['5192f819e0978987d662edabaf3143e1']=function()print('from subdirectories!')return true end;require('977c573b0324d59e85ec223a84801a3c')
+local proxy_package = {
+  loaded = {},
+  packages = {}
+}
+local _require = require
+
+function require(h)
+  if (proxy_package.loaded[h]) then
+    return proxy_package.loaded[h]
+  elseif (proxy_package.packages[h]) then
+    proxy_package.loaded[h] = proxy_package.packages[h]()
+    return proxy_package.loaded[h]
+  else
+    return _require(h)
+  end
+end
+
+proxy_package.packages['463841e40c49d6106ee19cbe3db6ad66'] = function()
+print('Hello world!')
+
+require('23da1b45a9c2eed6a4c8c4c62db4bfe2')
+require('23da1b45a9c2eed6a4c8c4c62db4bfe2')
+
+require('69bdfcba7dbe708f7f8e7271087002f9')
+end;
+
+proxy_package.packages['23da1b45a9c2eed6a4c8c4c62db4bfe2'] = function()
+print('hey there from module')
+
+return 'hey there'
+end;
+
+proxy_package.packages['69bdfcba7dbe708f7f8e7271087002f9'] = function()
+print('from subdirectories!')
+
+return true
+end;
+
+require('463841e40c49d6106ee19cbe3db6ad66')
